14e4b0b1371c5dd468dad40aec2615b7dde62844
Brandon Patterson <brandnpatterson@gmail.com>
2018-04-29 15:25:26 -0500

    remove console.log

--- src/js/custom/app.js
+++ src/js/custom/app.js
@@ -80,7 +80,6 @@ class Todos {
 
     delete(e) {
         let confirm = e.target.parentNode.lastChild.previousSibling;
-        console.log(confirm);
         if (confirm.classList.contains('btn-confirm')) {
             confirm.classList.toggle('hidden');
         }
